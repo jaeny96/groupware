123e98d5942f75672d6ec45bfce3e2bc768de7c8
jaeny96 <42085171+jaeny96@users.noreply.github.com>
2021-07-02 17:55:12 +0900

    .
    DAOOracle 완료

--- back/src/com/group/approval/dto/Reference.java
+++ back/src/com/group/approval/dto/Reference.java
@@ -7,42 +7,44 @@ import com.group.employee.dto.Employee;
 public class Reference {
 	public Document document_no;
 	public Employee employee_id;
-	public ApprovalStatus re_ap_type;
+	public ApprovalStatus ap_type;
 	
-//	public Document getDocument_no() {
-//		return document_no;
-//	}
-//	public void setDocument_no(Document document_no) {
-//		this.document_no = document_no;
-//	}
-//
-//	public Employee getEmployee_id() {
-//		return employee_id;
-//	}
-//	public void setEmployee_id(Employee employee_id) {
-//		this.employee_id = employee_id;
-//	}
-//	
-//	public Date getRe_ap_date() {
-//		return re_ap_date;
-//	}
-//	public void setRe_ap_date(Date re_ap_date) {
-//		this.re_ap_date = re_ap_date;
-//	}
-//	public String getRe_ap_comment() {
-//		return re_ap_comment;
-//	}
-//	public void setRe_ap_comment(String re_ap_comment) {
-//		this.re_ap_comment = re_ap_comment;
-//	}
-//	@Override
-//	public String toString() {
-//		return "Reference [document_no=" + document_no + ", employee_id=" + employee_id + ", re_ap_date=" + re_ap_date
-//				+ ", re_ap_comment=" + re_ap_comment + "]";
-//	}
-	
-	
-	
-	
+	public Reference() {
+	}
+
+	public Reference(Document document_no, Employee employee_id, ApprovalStatus ap_type) {
+		this.document_no = document_no;
+		this.employee_id = employee_id;
+		this.ap_type = ap_type;
+	}
+
+	public Document getDocument_no() {
+		return document_no;
+	}
+
+	public void setDocument_no(Document document_no) {
+		this.document_no = document_no;
+	}
+
+	public Employee getEmployee_id() {
+		return employee_id;
+	}
+
+	public void setEmployee_id(Employee employee_id) {
+		this.employee_id = employee_id;
+	}
+
+	public ApprovalStatus getAp_type() {
+		return ap_type;
+	}
+
+	public void setAp_type(ApprovalStatus ap_type) {
+		this.ap_type = ap_type;
+	}
+
+	@Override
+	public String toString() {
+		return "Reference [document_no=" + document_no + ", employee_id=" + employee_id + ", ap_type=" + ap_type + "]";
+	}
 	
 }
--- back/src/com/group/main/dao/MainDAOOracle.java
+++ back/src/com/group/main/dao/MainDAOOracle.java
@@ -10,6 +10,7 @@ import java.util.List;
 import com.group.approval.dto.Document;
 import com.group.board.dto.Board;
 import com.group.calendar.dto.Schedule;
+import com.group.employee.dto.Department;
 import com.group.employee.dto.Employee;
 import com.group.employee.dto.Leave;
 import com.group.sql.MyConnection;
@@ -88,12 +89,48 @@ public class MainDAOOracle implements MainDAO {
 
 	@Override
 	public List<Document> selectDocument(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		try {
+			con = MyConnection.getConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		String selectDocumentSQL = "SELECT document_no,document_title,draft_date\r\n"
+				+ "FROM (select rownum rn, a.*\r\n"
+				+ "    FROM ((SELECT d.document_title,d.document_no, draft_date, a.employee_id,ap_type \r\n"
+				+ "            FROM approval a JOIN document d ON a.document_no=d.document_no \r\n"
+				+ "            WHERE a.employee_id=? AND ap_type='대기')\r\n" + "            UNION ALL \r\n"
+				+ "          (SELECT d.document_title,d.document_no, draft_date, ag.employee_id,ap_type \r\n"
+				+ "           FROM agreement ag JOIN document d ON ag.document_no=d.document_no \r\n"
+				+ "           WHERE ag.employee_id=? AND ap_type='대기') ORDER BY draft_date asc)a\r\n" + "    )\r\n"
+				+ "WHERE rn BETWEEN 1 AND 5";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<Document> docList = new ArrayList<Document>();
+		try {
+			pstmt = con.prepareStatement(selectDocumentSQL);
+			pstmt.setString(1, id);
+			pstmt.setString(2, id);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Document doc = new Document();
+				doc.setDoucment_no(rs.getString("document_no"));
+				doc.setDocument_title(rs.getString("document_title"));
+				doc.setDraft_date(rs.getDate("draft_date"));
+
+				docList.add(doc);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			MyConnection.close(con, pstmt, rs);
+		}
+		return docList;
 	}
 
-	
-	//페이지 3 -> 10으로 나중에 변경
+	// 페이지 3 -> 10으로 나중에 변경
 	@Override
 	public List<Board> selectBoard() {
 		Connection con = null;
@@ -131,14 +168,49 @@ public class MainDAOOracle implements MainDAO {
 		} finally {
 			MyConnection.close(con, pstmt, rs);
 		}
-
 		return bdList;
 	}
 
 	@Override
 	public List<Schedule> selectSchedule(Employee emp) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		try {
+			con = MyConnection.getConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		String selectScheduleSQL = "SELECT skd_start_date ,skd_title\r\n"
+				+ "FROM (select skd_start_date ,skd_title, \r\n"
+				+ "    row_number() OVER (PARTITION BY e.employee_id ORDER BY skd_start_date ASC) rn\r\n"
+				+ "    FROM schedule s\r\n" + "    JOIN employee e ON s.employee_id=e.employee_id\r\n"
+				+ "    JOIN department d ON e.department_id=d.department_id\r\n"
+				+ "    WHERE ((d.department_id=? AND s.skd_share='t') OR (s.employee_id=? AND s.skd_share='p'))\r\n"
+				+ "    AND to_char(skd_start_date,'yyyy-mm-dd') =to_char(sysdate,'yyyy-mm-dd'))\r\n"
+				+ "WHERE rn BETWEEN 1 AND 5";
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<Schedule> skdList = new ArrayList<Schedule>();
+
+		try {
+			pstmt = con.prepareStatement(selectScheduleSQL);
+			pstmt.setString(1, emp.getDepartment().getDepartment_id());
+			pstmt.setString(2, emp.getEmployee_id());
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Schedule skd = new Schedule();
+				skd.setSkd_start_date(rs.getTimestamp("skd_start_date"));
+				skd.setSkd_title(rs.getString("skd_title"));
+
+				skdList.add(skd);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			MyConnection.close(con, pstmt, rs);
+		}
+		return skdList;
 	}
 
 	public static void main(String[] args) {
@@ -171,6 +243,36 @@ public class MainDAOOracle implements MainDAO {
 //		} catch (Exception e) {
 //			e.printStackTrace();
 //		}
+
+//		try {
+//			MainDAOOracle dao = new MainDAOOracle();
+//			String id = "CEO001";
+//			List<Document> docList = dao.selectDocument(id);
+//			for (Document doc : docList) {
+//				System.out.println(doc.getDoucment_no() + "/" + doc.getDocument_title() + "/" + doc.getDraft_date());
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+
+		try {
+			MainDAOOracle dao = new MainDAOOracle();
+			String id = "MSD002";
+			String depTitle = "MSD";
+			Employee emp = new Employee();
+			emp.setEmployee_id(id);;
+			Department dep = new Department();
+			dep.setDepartment_id(depTitle);
+			emp.setDepartment(dep);
+			List<Schedule> skdList = dao.selectSchedule(emp);
+			for (Schedule skd : skdList) {
+				System.out.println(skd.getSkd_start_date() + "/" + skd.getSkd_title());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 
 	}
 }
