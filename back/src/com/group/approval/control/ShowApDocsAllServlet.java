e17fddcca21fbc4387c38398ae334451102452e6
bmbkje <wjddms@ajou.ac.kr>
2021-07-28 08:11:31 +0900

    Modify sidebar list feature
    로그인  세션 추가

--- back/src/com/group/approval/control/ShowApDocsAllServlet.java
+++ back/src/com/group/approval/control/ShowApDocsAllServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.group.approval.dto.Document;
@@ -24,28 +25,23 @@ public class ShowApDocsAllServlet extends HttpServlet {
    //사이드 바를 누르면 전체 목록 보여주는 서블릿
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//나중에 페이지 요소 추가하기
-		//int currentPage = Integer.parseInt(request.getPa)
+		HttpSession session = request.getSession();
+		String id= session.getAttribute("id").toString();
 		
-		//전체 합치면 찐 id 넣기 
-		String loginId = request.getParameter("id");
-		
-		//요청전달데이터 얻기 
 		SideDocsService service;
 		ServletContext sc = getServletContext();
 		SideDocsService.envProp = sc.getRealPath(sc.getInitParameter("env"));
 		service = SideDocsService.getInstance();
 		
 		try {
-			List<Document> apDocsList = service.findDocsAll(loginId);
-			//json라이브러리로 json문자열 형태로 응답
+			List<Document> apDocsList = service.findDocsAll(id);
 			ObjectMapper mapper = new ObjectMapper();
-			//보낼때 데이터 포맷
+
 			mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
 			String jsonStr = mapper.writeValueAsString(apDocsList);
 			System.out.println(jsonStr);
 			response.setContentType("application/json;charset=utf-8");
-			//데이터 전달S
+
 			response.getWriter().print(jsonStr);
 		}catch(FindException e) {
 			e.printStackTrace();
